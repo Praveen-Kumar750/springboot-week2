6e56878ddc0ef4514f00909423f534bd6c5b4ed4
Praveen Kumar <praveenkumarpappu44@gmail.com>
2025-12-23 13:04:51 +0530

    Initial Commit for Springboot week-2

---
+++ src/main/java/com/praveen/springbootwebtutorial/controllers/EmployeeController.java
@@ -0,0 +1,71 @@
+package com.praveen.springbootwebtutorial.controllers;
+
+
+import com.praveen.springbootwebtutorial.dto.EmployeeDTO;
+import com.praveen.springbootwebtutorial.entities.EmployeeEntity;
+import com.praveen.springbootwebtutorial.repositories.EmployeeRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+//parent
+@RequestMapping(path="/employees")
+public class EmployeeController {
+
+//     @GetMapping(path="/getMySecretMessage")
+//    public String getMySecretMessage(){
+//         return "Secret Message: @#$%ghuygfc";
+//     }
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    //    @GetMapping("/employees/{employeeId}")
+@GetMapping("/{employeeId}")
+//    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
+//        return new EmployeeDTO(employeeId,"Praveen","praveen@gmail.com",21, LocalDate.of(2026,06,07),true);
+//    }
+//public EmployeeDTO getEmployeeById(@PathVariable(name="employeeId") Long id){
+//    return new EmployeeDTO(id,"Praveen","praveen@gmail.com",21, LocalDate.of(2026,06,07),true);
+//}
+
+public EmployeeEntity getEmployeeById(@PathVariable(name="employeeId") Long id){
+    return employeeRepository.findById(id).orElse(null);
+}
+
+
+//    @GetMapping(path="/employees")
+@GetMapping
+//    public String getAllEmployees(@RequestParam(required = false,name="inputAge")Integer age,
+//                                  @RequestParam(required = false)String sortBy){
+//        return "Hi age "+age+" "+sortBy;
+//    }
+public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false,name="inputAge")Integer age,
+                                            @RequestParam(required = false)String sortBy){
+    return employeeRepository.findAll();
+}
+@PostMapping
+//    public String createNewEmployee(){
+//    return "hello from post";
+//}
+//public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
+//    inputEmployee.setId(100L);
+//    return inputEmployee;
+//}
+
+public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity inputEmployee){
+    return employeeRepository.save(inputEmployee);
+}
+
+@PutMapping String updateEmployeeById(){
+    return "hello from put";
+}
+
+
+
+}
---
+++ src/main/java/com/praveen/springbootwebtutorial/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.praveen.springbootwebtutorial.repositories;
+
+import com.praveen.springbootwebtutorial.entities.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
+}
