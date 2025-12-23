08c2493764ddd920f4405cc25629e093431c29f4
Praveen Kumar <praveenkumarpappu44@gmail.com>
2025-12-23 13:38:15 +0530

    Main Update

--- src/main/java/com/praveen/springbootwebtutorial/SpringBootWebTutorialApplication.java
+++ src/main/java/com/praveen/springbootwebtutorial/SpringBootWebTutorialApplication.java
@@ -9,7 +9,7 @@ public class SpringBootWebTutorialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebTutorialApplication.class, args);
 
-		System.out.println("hello");
+		System.out.println("hi");
 	}
 
 }
